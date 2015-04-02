1dd578d65bf346359672f979da35c125c49693e5
Brian Shuve <brianshuve@109.triumfvisitor.net>
2015-04-02 13:13:25 -0700

    Added acknolwedgements, small updates

--- Report/2013_report/tarup.sh
+++ Report/2013_report/tarup.sh
@@ -1,1 +1,1 @@
-tar -cvzf boost2013_report.tgz boost2013_report.tex introduction.tex samples.tex multivariate.tex substructure.tex qgtagging.tex wtagging.tex toptagging.tex conclusions.tex boost2013_report.bib Figures Makefile spphys.bst svjour3.cls boost2013_report.pdf svepjc3.clo svglov3.clo 
+tar -cvzf boost2013_report.tgz boost2013_report.tex introduction.tex samples.tex multivariate.tex substructure.tex qgtagging.tex wtagging.tex toptagging.tex conclusions.tex boost2013_report.bib boost2013_report.bbl Figures Makefile spphys.bst svjour3.cls boost2013_report.pdf svepjc3.clo svglov3.clo 
